78f58eb4625a148b8b9f5c9988c67581279770b2
DaniaAbughoush <ms1dodo@gmail.com>
2021-05-21 21:10:20 +0300

    new book

--- models/User.js
+++ models/User.js
@@ -1,21 +1,19 @@
-'use strict';
-
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+
 
 const bookSchema = new Schema({
   name: String,
   description: String,
   status: String,
+  photo: String
 });
 
 const userSchema = new Schema({
   email: String,
-  books: [bookSchema],
+  books: [bookSchema]
 });
 
-// make model from schema
 const User = mongoose.model('User', userSchema);
 
-// export the model
 module.exports = User;
